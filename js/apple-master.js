5e1be9f372ac754e4cd7891334ac24525e7108c0
Bahi <blackfoxpredictor@gmail.com>
2026-08-01 22:14:10 +0300

    new script

--- js/apple-master.js
+++ js/apple-master.js
@@ -2,7 +2,7 @@ window.TOOL_CONFIG = Object.freeze({
     name: "APPLE MASTER",
 
     downloadUrl:
-        "https://download.noxir.org/apple-master/AppleMaster.apk",
+        "https://download.noxir.org/apple-master-v1/Apple-Master.apk",
 
     videoUrl:
         "https://download.noxir.org/apple-master-v1/how-to-use-apple-master.mp4",
